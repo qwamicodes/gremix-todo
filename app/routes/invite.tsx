ce93fbb4296043ff664ddb89411c716d51d19c12
De-Great Yartey <mail@degreat.co.uk>
2025-05-26 04:58:38 +0000

    fix bug with token generation

--- app/routes/invite.tsx
+++ app/routes/invite.tsx
@@ -2,19 +2,17 @@ import { addHours } from "date-fns";
 import { customAlphabet } from "nanoid";
 import { prisma } from "~/lib/prisma.server";
 
-const token = customAlphabet(
+const generateToken = customAlphabet(
 	"abcde0123456789fghijklmnABCDEFGHNOPopqrstWXYZuvwxyz",
 	10,
 );
 
 export const loader = async () => {
 	const expiresAt = addHours(new Date(), 12);
+	const token = generateToken();
 
 	await prisma.inviteToken.create({
-		data: {
-			token: token(),
-			expiresAt,
-		},
+		data: { token, expiresAt },
 	});
 
 	return { token };
